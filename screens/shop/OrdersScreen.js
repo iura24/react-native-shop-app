0c863bb291ca4e98739759d4b837d04cd85a033a
iura <iura.gurulea@gmail.com>
2020-04-28 15:38:39 +0300

    cleaning cart and styling order items

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import { Text, FlatList, Platform } from "react-native";
+import { Text, FlatList, Platform, View } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
+
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => {
-        <Text>{itemData.item.totalAmount}</Text>;
-      }}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
     />
   );
 };
