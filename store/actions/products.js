2e0c6dd0e061fd809525bcf8f253c0242731882a
iura <iura.gurulea@gmail.com>
2020-05-04 12:44:00 +0300

    storing and fetching stored orders

--- store/actions/products.js
+++ store/actions/products.js
@@ -40,12 +40,15 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-shop-app-3387e.firebaseio.com/products/${productId}.json`,
       {
         method: "DELETE",
       }
     );
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
 };
@@ -85,7 +88,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async (dispatch) => {
-    await fetch(
+    const response = await fetch(
       `https://rn-shop-app-3387e.firebaseio.com/products/${id}.json`,
       {
         method: "PATCH",
@@ -99,6 +102,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         }),
       }
     );
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
