e9044102b484811a80ce2c2ec8196b4e0f82ab8f
iura <iura.gurulea@gmail.com>
2020-05-04 17:19:27 +0300

    mapping orders to user and implementing auto login

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -4,10 +4,11 @@ export const SET_ORDERS = "SET_ORDERS";
 import Order from "../../models/order";
 
 export const fetchOrders = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
     try {
       const response = await fetch(
-        "https://rn-shop-app-3387e.firebaseio.com/orders/u1.json"
+        `https://rn-shop-app-3387e.firebaseio.com/orders/${userId}.json`
       );
 
       if (!response.ok) {
@@ -38,9 +39,10 @@ export const fetchOrders = () => {
 export const addOrder = (cartItems, totalAmount) => {
   return async (dispatch, getState) => {
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const date = new Date();
     const response = await fetch(
-      `https://rn-shop-app-3387e.firebaseio.com/orders/u1.json?auth${token}`,
+      `https://rn-shop-app-3387e.firebaseio.com/orders/${userId}.json?auth=${token}`,
       {
         method: "POST",
         headers: {
--- store/actions/products.js
+++ store/actions/products.js
@@ -6,7 +6,9 @@ export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const SET_PRODUCTS = "SET_PRODUCTS";
 
 export const fetchProducts = () => {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
+    const userId = getState().auth.userId;
+
     try {
       const response = await fetch(
         "https://rn-shop-app-3387e.firebaseio.com/products.json"
@@ -23,7 +25,7 @@ export const fetchProducts = () => {
         loadedProducts.push(
           new Product(
             key,
-            "u1",
+            resData[key].ownerId,
             resData[key].title,
             resData[key].imageUrl,
             resData[key].description,
@@ -31,7 +33,11 @@ export const fetchProducts = () => {
           )
         );
       }
-      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+      dispatch({
+        type: SET_PRODUCTS,
+        products: loadedProducts,
+        userProducts: loadedProducts.filter((prod) => prod.ownerId === userId),
+      });
     } catch (err) {
       throw err;
     }
@@ -57,6 +63,7 @@ export const deleteProduct = (productId) => {
 export const createProduct = (title, description, imageUrl, price) => {
   return async (dispatch, getState) => {
     const token = getState().auth.token;
+    const userId = getState().auth.userId;
     const response = await fetch(
       `https://rn-shop-app-3387e.firebaseio.com/products.json?auth=${token}`,
       {
@@ -69,6 +76,7 @@ export const createProduct = (title, description, imageUrl, price) => {
           description,
           imageUrl,
           price,
+          ownerId: userId,
         }),
       }
     );
@@ -83,6 +91,7 @@ export const createProduct = (title, description, imageUrl, price) => {
         description,
         imageUrl,
         price,
+        ownerId: userId,
       },
     });
   };
