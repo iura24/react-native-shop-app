b5146dc352cfbd339fcf9bd9fece8c8246862e01
iura <iura.gurulea@gmail.com>
2020-05-06 15:32:07 +0300

    getting user location and showing a  map preview

---
+++ env.js
@@ -0,0 +1,17 @@
+const variables = {
+  development: {
+    googleApiKey: "",
+  },
+  production: {
+    googleApiKey: "",
+  },
+};
+
+const getEnvVariables = () => {
+  if (__DEV__) {
+    return variables.development;
+  }
+  return variables.production;
+};
+
+export default getEnvVariables;
